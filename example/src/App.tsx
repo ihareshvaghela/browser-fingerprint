099666e36365c315a4be1299c94eec39fbb6a5a2
rajesh-edx <r.royal@edexa.team>
2022-04-19 06:49:07 +0530

    preview with social media preview added

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import { getDeviceId } from '@rajesh896/browser-fingerprint';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [browserFingerprint, setBrowserFingerPrint] = useState("");
+  const getBrowserFingerPrint = () => {
+    getDeviceId().then((fingerprint) => {
+    setBrowserFingerPrint(fingerprint)
+    }).catch((err) => {
+    setBrowserFingerPrint(err)
+  })
+ }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div className='header'>
+        <header >
+        Browser FingerPrint
       </header>
+      <p>This package generates a unique ID/String for different browsers. Like chrome, firefox and any other browser which supports <b>canvas fingerPrinting</b> and <b>audio FingerPrinting</b>.</p>
+      </div>
+      <section className="id-container">
+        <textarea rows={3} cols={100} placeholder="This browser's fingerprint" value={browserFingerprint}></textarea>
+        <p><button onClick={() => {getBrowserFingerPrint()}}>Generate FingerPrint</button></p>
+        <p style={{marginTop: 20}}>You must try this window in incognito, with vpn and check if Id remains same. It will never change.</p>
+      </section>
+      <footer>
+        <div><a href='https://github.com/rajesh-royal' title='developer/author github account'>@rajesh-royal</a></div>
+        <div><a title='go to github link of this repo' href='https://github.com/Rajesh-Royal/browser-fingerprint'>GitHub</a></div>
+      </footer>
     </div>
   );
 }
